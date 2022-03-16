080bbb55c4c550f46cc8b56d9e31273b8430e19f
slarkdarr <daffaresyaly@gmail.com>
2022-03-16 21:34:37 +0700

    Fix bugs on file path

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 from PIL import Image
 
 def rotate_image(img):
-    img.rotate(-90)                 # Rotate image 90 degrees clockwise
+    return img.rotate(-90)          # Rotate image 90 degrees clockwise
 
 def resize_image(img):
     return img.resize((128, 128))   # Resize image to 128x128
@@ -11,27 +11,24 @@ def convert_to_rgb(img):
     return img.convert('RGB')       # Convert image to RGB so it can be saved as JPEG
 
 def save(img, outfile):
-    img.save(outfile, 'JPEG')       # Save image with JPEG format
+    img.save(outfile, 'JPEG')
 
 def process_image(filename, directory):
     infile = os.path.join(directory, filename)          # Input file appended with the directory
-    outfile = os.path.join('./opt/icons', filename)     # Output file appended with the directory
+    outfile = os.path.join('/opt/icons', filename)      # Output file appended with the directory
 
     # Image is processed here
     with Image.open(infile) as img:
-        rotate_image(img)
-        img_resized = resize_image(img)
+        img_rotated = rotate_image(img)
+        img_resized = resize_image(img_rotated)
         img_converted = convert_to_rgb(img_resized)
         save(img_converted, outfile)
 
 def main():
-    directory = './images'           # Images folder
+    directory = 'images/'    # Images folder
     for filename in os.listdir(directory):
-        try:
+        if filename.startswith('ic_'):
             process_image(filename, directory)
-            break
-        except:
-            pass
 
 if __name__ == '__main__':
     main()
