289821af35727d29aa142cc7938be7424d98a9e1
slarkdarr <daffaresyaly@gmail.com>
2022-03-16 21:42:56 +0700

    Add second approach of processing image

--- main.py
+++ main.py
@@ -13,6 +13,7 @@ def convert_to_rgb(img):
 def save(img, outfile):
     img.save(outfile, 'JPEG')
 
+# First approach (Process image using the functions made myself)
 def process_image(filename, directory):
     infile = os.path.join(directory, filename)          # Input file appended with the directory
     outfile = os.path.join('/opt/icons', filename)      # Output file appended with the directory
@@ -24,11 +25,21 @@ def process_image(filename, directory):
         img_converted = convert_to_rgb(img_resized)
         save(img_converted, outfile)
 
+# Second approach (Process image directly with the functions from library)
+def process_image2(filename, directory):
+    infile = os.path.join(directory, filename)          # Input file appended with the directory
+    outfile = os.path.join('/opt/icons', filename)      # Output file appended with the directory
+
+    # Image is processed here
+    with Image.open(infile) as img:
+        img.rotate(-90).resize((128, 128)).convert('RGB').save(outfile, 'JPEG')
+
 def main():
     directory = 'images/'    # Images folder
     for filename in os.listdir(directory):
         if filename.startswith('ic_'):
             process_image(filename, directory)
+            # process_image2(filename, directory)
 
 if __name__ == '__main__':
     main()
